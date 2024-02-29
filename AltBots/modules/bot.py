e0b5c41407e52c3cc01545604d370102774a045f
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-02-29 23:30:00 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -99,7 +99,7 @@ async def addsudo(event):
         Heroku = heroku3.from_key(HEROKU_API_KEY)
         sudousers = getenv("SUDO_USERS", default=None)
 
-        ok = await event.reply(f"» __Jarvis Ka Ek Beta Aur Add Ho rha hai..__")
+        ok = await event.reply(f"» 🇴𝗫𝗚𝗘𝗡 𝗡𝗘𝗪 𝗦𝗨𝗗𝗢 𝗨𝗦𝗘𝗥 𝗔𝗗𝗗𝗘𝗗__")
         target = ""
         if HEROKU_APP_NAME is not None:
             app = Heroku.app(HEROKU_APP_NAME)
