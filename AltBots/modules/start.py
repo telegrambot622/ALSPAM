08854983a272fd4edd292853156609609f3c2dd1
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-03-01 02:03:44 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -1,7 +1,6 @@
-from telethon import __version__, events, Button
+from telethon import version, events, Button
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
-
 
 START_BUTTON = [
     [
@@ -16,14 +15,12 @@ START_BUTTON = [
     ]
 ]
 
-
 @X1.on(events.NewMessage(pattern="/start"))
 @X2.on(events.NewMessage(pattern="/start"))
 @X3.on(events.NewMessage(pattern="/start"))
 @X4.on(events.NewMessage(pattern="/start"))
 @X5.on(events.NewMessage(pattern="/start"))
 @X6.on(events.NewMessage(pattern="/start"))
-@X7.on(events.NewMessage(pattern="/start"))
 @X7.on(events.NewMessage(pattern="/start"))
 @X8.on(events.NewMessage(pattern="/start"))
 @X9.on(events.NewMessage(pattern="/start"))
@@ -33,11 +30,17 @@ async def start(event):
         AltBot = await event.client.get_me()
         bot_name = AltBot.first_name
         bot_id = AltBot.id
-        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗢𝗫𝗬𝗚𝗘𝗡](https://t.me/PRADHAN474)**\n\n"
-        TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
-        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        TEXT = (
+            "╔═════════════════════╗\n"
+            "║ ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id}) ║\n"
+            "║━━━━━━━━━━━━━━━━━━━║\n"
+            "║ ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ : [𝗢𝗫𝗬𝗚𝗘𝗡](https://t.me/PRADHAN474) ║\n"
+            "║━━━━━━━━━━━━━━━━━━━║\n"
+            "║ xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ : M3.3 ║\n"
+            "║ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 3.11.3 ║\n"
+            "║ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : {version} ║\n"
+            "╚═════════════════════╝"
+        )
         await event.client.send_file(
                     event.chat_id,
                     "https://te.legra.ph/file/7bc5551374ee010ba57d2.jpg",
