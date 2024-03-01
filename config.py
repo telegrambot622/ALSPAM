95e3e77913edd46b17b6f6c23d2d216efacb7628
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-03-01 13:24:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from telethon import TelegramClient
 
 from os import getenv
-from Oxyspam.data import ALTRON
+from AltBots.data import ALTRON
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
